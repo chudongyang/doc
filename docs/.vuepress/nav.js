eb528a2b567d929092ab475aa15038ea4275412a
chudongyang <315039355@qq.com>
2019-07-16 00:49:17 +0800

    ts:类

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,5 +1,5 @@
 module.exports = [
-	{ text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
+	{ text: '掘金专栏', link: 'https://juejin.im/user/586687fe61ff4b006db52c79/posts' }, // 内部链接 以docs为根目录
 	{ text: '博客', link: 'https://chudongyang.github.io/' }, // 外部链接
 	// 下拉列表
 	{
